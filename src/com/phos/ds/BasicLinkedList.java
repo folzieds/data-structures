78a923a536254423f3a2dca02886f4ba68d7346a
Adefolarin Osu <aosu@teamapt.com>
2020-06-10 15:54:14 +0100

    Added size, add,remove, insert at, remove at methods to the Basic Linked list

--- src/com/phos/ds/BasicLinkedList.java
+++ src/com/phos/ds/BasicLinkedList.java
@@ -4,10 +4,81 @@ public class BasicLinkedList<X> {
 
     private Node first;
     private Node last;
+    private int nodeCount;
 
     public BasicLinkedList(){
         first=null;
         last=null;
+        nodeCount=0;
+    }
+
+    public int size(){
+        return nodeCount;
+    }
+
+    public void add(X item){
+        if(first == null){
+            first = new Node(item);
+            last = first;
+        }
+        else{
+            Node newNode = new Node(item);
+            last.setNextNode(newNode);
+            last = newNode;
+        }
+        nodeCount++;
+    }
+
+    public X remove(){
+        if (first == null)
+            throw new IllegalStateException("The linked list is empty and there are no items to remove");
+
+        X nodeItem = first.nodeItem;
+
+        // update the first pointer to the next node item
+        first = first.getNextNode();
+        nodeCount--;
+
+        return nodeItem;
+    }
+
+    public void insert(X item, int position){
+        if (size() < position)
+            throw new IllegalStateException("The position chosen is greater than the size of the LinkedList");
+
+        Node currentNode = first;
+
+        // we are starting at 1 because we are already on the first node
+        // we loop through the list till we get to the position on the list
+        for(int i = 1; i < position && currentNode.getNextNode() != null; i++)
+            currentNode = currentNode.getNextNode();
+
+        Node newNode = new Node(item);
+        newNode.setNextNode(currentNode.getNextNode());
+        currentNode.setNextNode(newNode);
+        nodeCount++;
+
+    }
+
+    public X removeAt(int position){
+        if(size() < position)
+            throw new IllegalStateException("The position chosen is greater than the size of the Linked list");
+
+        Node previousNode = first;
+        Node currentNode = first;
+
+        // we are starting at 1 because we are already on the first node
+        // we loop through the list till we get to the position on the list
+        for(int i = 1; i < position && currentNode.getNextNode() != null; i++){
+            previousNode = currentNode;
+            currentNode = currentNode.getNextNode();
+        }
+
+        X nodeItem = currentNode.nodeItem;
+        previousNode.setNextNode(currentNode.getNextNode());
+
+        return nodeItem;
+
     }
 
     private class Node {
